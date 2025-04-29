ee40ede6f9213d6461c0ae1173ebb8e2e2032515
anshul-dhomne <anshul.dhomnee@gmail.com>
2025-04-29 19:49:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  
-}
+function mincost(arr) {
+    let totalCost = 0;
 
-module.exports=mincost;
+    while (arr.length > 1) {
+        // Sort array to get the two smallest ropes
+        arr.sort((a, b) => a - b);
+
+        // Take the two smallest ropes
+        let first = arr.shift();
+        let second = arr.shift();
+
+        // Combine them and calculate cost
+        let cost = first + second;
+        totalCost += cost;
+
+        // Add the combined rope back to the array
+        arr.push(cost);
+    }
+
+    return totalCost;
+}
