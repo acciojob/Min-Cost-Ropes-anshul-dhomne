7a04dc496c7e6db4f73bfafecadd30e856cead70
anshul-dhomne <anshul.dhomnee@gmail.com>
2025-04-29 19:57:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,72 @@
+class MinHeap {
+    constructor() {
+        this.heap = [];
+    }
+
+    insert(val) {
+        this.heap.push(val);
+        this.bubbleUp();
+    }
+
+    bubbleUp() {
+        let index = this.heap.length - 1;
+        while (index > 0) {
+            let parentIndex = Math.floor((index - 1) / 2);
+            if (this.heap[parentIndex] <= this.heap[index]) break;
+            [this.heap[parentIndex], this.heap[index]] = [this.heap[index], this.heap[parentIndex]];
+            index = parentIndex;
+        }
+    }
+
+    extractMin() {
+        if (this.heap.length === 1) return this.heap.pop();
+        const min = this.heap[0];
+        this.heap[0] = this.heap.pop();
+        this.sinkDown();
+        return min;
+    }
+
+    sinkDown() {
+        let index = 0;
+        const length = this.heap.length;
+        const element = this.heap[0];
+
+        while (true) {
+            let left = 2 * index + 1;
+            let right = 2 * index + 2;
+            let smallest = index;
+
+            if (left < length && this.heap[left] < this.heap[smallest]) {
+                smallest = left;
+            }
+            if (right < length && this.heap[right] < this.heap[smallest]) {
+                smallest = right;
+            }
+            if (smallest === index) break;
+            [this.heap[index], this.heap[smallest]] = [this.heap[smallest], this.heap[index]];
+            index = smallest;
+        }
+    }
+
+    size() {
+        return this.heap.length;
+    }
+}
+
 function mincost(arr) {
+    const heap = new MinHeap();
+    for (let num of arr) {
+        heap.insert(num);
+    }
+
     let totalCost = 0;
 
-    while (arr.length > 1) {
-        // Sort array to get the two smallest ropes
-        arr.sort((a, b) => a - b);
-
-        // Take the two smallest ropes
-        let first = arr.shift();
-        let second = arr.shift();
-
-        // Combine them and calculate cost
+    while (heap.size() > 1) {
+        let first = heap.extractMin();
+        let second = heap.extractMin();
         let cost = first + second;
         totalCost += cost;
-
-        // Add the combined rope back to the array
-        arr.push(cost);
+        heap.insert(cost);
     }
 
     return totalCost;
