ff6c16fbac16dd896c62a9053afa6f4fbfa77257
anshul-dhomne <anshul.dhomnee@gmail.com>
2025-05-07 19:16:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,73 +1,47 @@
 function mincost(arr) {
-    // Min Heap implementation using JavaScript
-    class MinHeap {
-        constructor() {
-            this.heap = [];
-        }
+    let totalCost = 0;
+    let heap = new MinHeap(arr); // Assume MinHeap is implemented
 
-        insert(val) {
-            this.heap.push(val);
-            this._bubbleUp();
-        }
-
-        extractMin() {
-            if (this.heap.length === 1) return this.heap.pop();
-            const min = this.heap[0];
-            this.heap[0] = this.heap.pop();
-            this._bubbleDown();
-            return min;
-        }
-
-        _bubbleUp() {
-            let index = this.heap.length - 1;
-            while (index > 0) {
-                let parentIndex = Math.floor((index - 1) / 2);
-                if (this.heap[parentIndex] <= this.heap[index]) break;
-                [this.heap[parentIndex], this.heap[index]] = [this.heap[index], this.heap[parentIndex]];
-                index = parentIndex;
-            }
-        }
-
-        _bubbleDown() {
-            let index = 0;
-            const length = this.heap.length;
-            while (true) {
-                let left = 2 * index + 1;
-                let right = 2 * index + 2;
-                let smallest = index;
-
-                if (left < length && this.heap[left] < this.heap[smallest]) smallest = left;
-                if (right < length && this.heap[right] < this.heap[smallest]) smallest = right;
-
-                if (smallest === index) break;
-                [this.heap[smallest], this.heap[index]] = [this.heap[index], this.heap[smallest]];
-                index = smallest;
-            }
-        }
-
-        size() {
-            return this.heap.length;
-        }
-    }
-
-    const heap = new MinHeap();
-    for (let length of arr) {
-        heap.insert(length);
-    }
+    while (heap.size() > 1) {function mincost(arr) {
+    // Create a min-heap using a priority queue
+    let heap = [...arr];
+    heap.sort((a, b) => a - b);  // initial sort
 
     let totalCost = 0;
 
-    while (heap.size() > 1) {
-        const first = heap.extractMin();
-        const second = heap.extractMin();
-        const cost = first + second;
+    while (heap.length > 1) {
+        // Pop two smallest elements
+        let first = heap.shift();
+        let second = heap.shift();
+
+        // Combine them and add cost
+        let cost = first + second;
         totalCost += cost;
-        heap.insert(cost);
+
+        // Insert the combined rope back in sorted order
+        // Binary insert for efficiency
+        let inserted = false;
+        for (let i = 0; i < heap.length; i++) {
+            if (heap[i] > cost) {
+                heap.splice(i, 0, cost);
+                inserted = true;
+                break;
+            }
+        }
+        if (!inserted) {
+            heap.push(cost);
+        }
     }
 
     return totalCost;
 }
 
-// Example usage:
-console.log(mincost([4, 3, 2, 6])); // Output: 29
-console.log(mincost([1, 2, 3, 4, 5])); // Output: 33
+        let first = heap.pop();
+        let second = heap.pop();
+        let cost = first + second;
+        totalCost += cost;
+        heap.push(cost);
+    }
+
+    return totalCost;
+}
